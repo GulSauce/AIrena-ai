ac2fc489b915c059ebda3172fcfb7f05f23b9516
Oh YoungJe <pine7420@naver.com>
2025-03-14 19:05:33 +0900

    Hotfix: 검색엔진 제거

--- app/core/util/serach_engine.py
+++ app/core/util/serach_engine.py
@@ -3,11 +3,3 @@ from langchain_community.tools import DuckDuckGoSearchRun
 
 def search_with_subject(subject):
     searched_result = ""
-    try:
-        search = DuckDuckGoSearchRun()
-        searched_result = search.invoke(subject + " 나무위키")
-        print(searched_result)
-    except:
-        searched_result = ""
-    finally:
-        return searched_result
